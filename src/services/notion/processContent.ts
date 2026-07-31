e9d75cc2f28f0eef692abbb0574fcc8469793014
Larissa Bittar <larissa.bittar@kiwify.com.br>
2026-07-31 14:00:22 -0300

    sidebar processos11

--- src/services/notion/processContent.ts
+++ src/services/notion/processContent.ts
@@ -4,10 +4,19 @@ export async function getProcessBlocks(
   pageId: string
 ) {
 
+  console.log("PAGE ID:", pageId);
+
+  const page = await notion.pages.retrieve({
+    page_id: pageId,
+  });
+
+  console.log("PAGE:", JSON.stringify(page, null, 2));
+
   const blocks = await notion.blocks.children.list({
     block_id: pageId,
-    page_size: 100,
   });
+
+  console.log("BLOCKS:", JSON.stringify(blocks, null, 2));
 
   return blocks.results;
 
